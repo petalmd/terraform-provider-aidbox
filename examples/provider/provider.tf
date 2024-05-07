450cd51edb9fe5ac37ade0e02ede8f375f8fa1a7
Guillaume Veschambre <gveschambre@petal-health.com>
2024-05-07 12:29:03 -0400

    Add license resource
    Signed-off-by: Guillaume Veschambre <gveschambre@petal-health.com>

--- examples/provider/provider.tf
+++ examples/provider/provider.tf
@@ -1,3 +1,13 @@
-provider "scaffolding" {
-  # example configuration here
+terraform {
+  required_providers {
+    aidbox = {
+      source  = "petalmd/aidbox"
+      version = "~> 0.0.1"
+    }
+  }
 }
+
+provider "aidbox" {
+  endpoint = "https://aidbox.app/rpc"
+}
+
